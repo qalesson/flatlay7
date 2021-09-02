480d9748c13f5b11078b508abe75355f80400479
Sergii Khromchenko <khromchenko13@gmail.com>
2021-09-02 11:19:00 -0700

    update capabilities and specs path

--- wdio.conf.js
+++ wdio.conf.js
@@ -21,7 +21,7 @@ exports.config = {
     // will be called from there.
     //
     specs: [
-        './specs/**/*.js'
+        './specs/**/**/*.js'
     ],
     // Patterns to exclude.
     exclude: [
@@ -50,14 +50,19 @@ exports.config = {
     // https://docs.saucelabs.com/reference/platforms-configurator
     //
     capabilities: [{
-    
         // maxInstances can get overwritten per capability. So if you have an in-house Selenium
         // grid with only 5 firefox instances available you can make sure that not more than
         // 5 instances get started at a time.
         maxInstances: 5,
         //
         browserName: 'chrome',
-        acceptInsecureCerts: true
+        'goog:chromeOptions': {
+            w3c: false,
+            args: ['window-size=1920,1080', // - full screen size
+                // 'headless', // - run automation with or without actual browser. Headless === without
+                "no-sandbox", // - this flag is needed in order to run automation in docker container
+                "disable-gpu"] // - this flag is needed in order to run automation in docker container
+        }
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
@@ -94,7 +99,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'http://localhost',
+    baseUrl: 'https://new.flatlay.io',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
