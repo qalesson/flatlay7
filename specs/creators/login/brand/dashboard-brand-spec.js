ed71cdff6ef51760f2c0b32e90442eaa589723f8
kosarsute <kosarsute@gmail.com>
2021-09-08 18:15:48 -0700

    added space

--- specs/creators/login/brand/dashboard-brand-spec.js
+++ specs/creators/login/brand/dashboard-brand-spec.js
@@ -4,26 +4,30 @@ const dashboardBrandsPage = require('../../../../page_objects/brands/dashboard-b
 describe('Login - Brand', () => {
     beforeEach(function () {
         LoginPage.login({ email: 'qalesson@gmail.com', password: 'qalesson@gmail.com', portal: 'brands' });
-   
     });
+
     it('Should get redirected to discover page upon click on discover', () => {
         dashboardBrandsPage.$discoverButton.waitForClickable();
         dashboardBrandsPage.$discoverButton.click();
         dashboardBrandsPage.$confirmDiscoverPage.waitForDisplayed();
     });
+
     it('Should get redirected to campaigns page upon click on campaigns', () => {
         dashboardBrandsPage.$campaignsButton.waitForClickable()
         dashboardBrandsPage.$campaignsButton.click();
         dashboardBrandsPage.$confirmCampaignsPage.waitForDisplayed();
     });
+
     it('Should get redirected to saved page upon click on saved', () => {
         dashboardBrandsPage.$savedButton.waitForClickable();
         dashboardBrandsPage.$savedButton.click();
         dashboardBrandsPage.$confirmSavedPage.waitForDisplayed();
     });
+
     it('Should get redirected to home page upon click on home', () => {
         dashboardBrandsPage.$homeButton.waitForClickable();
         dashboardBrandsPage.$homeButton.click();
         dashboardBrandsPage.$confirmHomePage.waitForDisplayed();
     });
+    
 })
