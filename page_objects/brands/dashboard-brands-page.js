255ae0fe6a3f8a30ad50b108c6213276638d9bfa
Сергей Сандул <sergeysandul@MBP-Sergej.attlocal.net>
2021-09-08 17:41:24 -0700

    FL-67

--- page_objects/brands/dashboard-brands-page.js
+++ page_objects/brands/dashboard-brands-page.js
@@ -4,6 +4,8 @@ class Dashboard {
     get $accountSettingsLnk() {return $('[routerlink="/creator/settings"]');}
     get upgradeBtn() {return $('[alt="Upgrade"]');}
     get pricingList() {return $('[class="row pricing-plan"]');}
+    get yearlyPlansBtn() {return $('[class="p-2 text-center"]');}
+    get premiumPlanTxt() {return $('[class="card-header pricing-prem-plan d-flex flex-row justify-content-between align-center"]');}
     
 }
 module.exports = new Dashboard();
--- specs/brands/dashboard.js
+++ specs/brands/dashboard.js
@@ -3,7 +3,7 @@ const DashboardPage = require('../../page_objects/creators/dashboard-creators-pa
 const DashboardPageBrands = require('../../page_objects/brands/dashboard-brands-page');
 const expect = require('chai').expect;
 
-describe('Login - Creators', () => {
+describe('Dashboard - Brands', () => {
     it('User should be able to see 4 monthly plans on "Plans" page', () => {
         LoginPage.login({ email: 'qalesson@gmail.com', password: 'qalesson@gmail.com', portal: 'brands' });
         // DashboardPageBrands.$accountSettingsLnk.waitForDisplayed({timeoutMsg: 'User was not able to login'});
@@ -15,6 +15,20 @@ describe('Login - Creators', () => {
         expect(plan).to.contain('PRO');
         expect(plan).to.contain('PREMIUM');
         expect(plan).to.contain('ENTERPRISE');
+
+    }),
+    it('User should be able to see 4 yearly plans on "Plans" page', () => {
+        LoginPage.login({ email: 'qalesson@gmail.com', password: 'qalesson@gmail.com', portal: 'brands' });
+        // DashboardPageBrands.$accountSettingsLnk.waitForDisplayed({timeoutMsg: 'User was not able to login'});
+        
+        DashboardPageBrands.upgradeBtn.waitForDisplayed();
+        DashboardPageBrands.upgradeBtn.click();
+        DashboardPageBrands.yearlyPlansBtn.waitForDisplayed();
+        DashboardPageBrands.yearlyPlansBtn.click();
+
+        let yearlyPlan = DashboardPageBrands.pricingList.getText();
+        expect(yearlyPlan).to.contain('$1,000');
+        expect(yearlyPlan).to.contain('$3,300');
 
     })
 })
