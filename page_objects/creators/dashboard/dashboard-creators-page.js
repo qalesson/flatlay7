24dc41b918f59987590d80213197da5f2cdaaeeb
Anastasiia Seredina <anastasiiaseredina@MBP-Anastasiia.attlocal.net>
2021-09-10 17:00:24 -0500

    Creators dashboard spec with test cases FL-5, 6, 7 added

--- page_objects/creators/dashboard/dashboard-creators-page.js
+++ page_objects/creators/dashboard/dashboard-creators-page.js
@@ -3,5 +3,15 @@ class Dashboard {
     // Reusable selector getters that will help us to avoid selector duplicates
     get $accountSettingsLnk() {return $('[routerlink="/creator/settings"]');};
     get searchBtn() {return $('span=Search');};
+    get $viewPublicProfileBtn() {return $('[class="top-main"] div:nth-child(2)');};
+    get $collectionsBtn() {return $('[class="item d-flex align-items-end"]');};
+    get $noPostsTxt() {return $('span=There are no posts here yet');};
+    get $noCollectionsTxt() {return $('span=There are no collections here yet');};
+
+    switchToOpenTab(mainTab) {
+        browser.waitUntil(() => browser.getWindowHandles().length > 1);
+        const popupWindow = browser.getWindowHandles().filter((handle) => handle !== mainTab)[1];
+        browser.switchToWindow(popupWindow);
+    };
 }
 module.exports = new Dashboard();
---
+++ specs/creators/dashboard/dashboard-creators-spec.js
@@ -0,0 +1,37 @@
+const LoginPage = require('../../../page_objects/login-page');
+const DashboardPage = require('../../../page_objects/creators/dashboard/dashboard-creators-page');
+const Credentials = require("../../../data/credentials.json");
+
+const { expect } = require("chai");
+
+const email = Credentials.creators.dashboard.email;
+const password = Credentials.creators.dashboard.password;
+const viewPublicProfileUrl = 'https://new.flatlay.io/profile/nastia'
+
+describe("Dashboard", () => {
+    beforeEach(() => {
+        // Go to login page and login as a Creator
+        LoginPage.login({ email: email, password: password });
+        DashboardPage.$accountSettingsLnk.waitForDisplayed({ timeoutMsg: 'User was not able to login' });
+    });
+    it('Should see amount of Posts, Following, Followers, Collections FL-5', () => {
+        $$('[class="m-0 amount font-gt"]').forEach(element => {
+            expect(parseInt(element.getText())).to.be.a("number");
+        })
+    });
+
+    it('Should see no posts, no collections text if none exist FL-6', () => {
+        const noPosts = DashboardPage.$noPostsTxt.getText()
+        expect(noPosts).to.equal('There are no posts here yet');
+        DashboardPage.$collectionsBtn.click();
+        DashboardPage.$noCollectionsTxt.waitForDisplayed;
+        const noCollections = DashboardPage.$noCollectionsTxt.getText()
+        expect(noCollections).to.equal('There are no collections here yet');
+    });
+
+    it('Should be able to view public profile FL-7', () => {
+        DashboardPage.$viewPublicProfileBtn.click();
+        DashboardPage.switchToOpenTab();
+        expect(browser.getUrl()).to.equal(viewPublicProfileUrl);
+    });
+})
