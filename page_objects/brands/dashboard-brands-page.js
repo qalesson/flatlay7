cbc600a4c9b67042905c7f265c6b3e7b93a3a5db
Сергей Сандул <sergeysandul@MBP-Sergej.attlocal.net>
2021-09-10 09:33:31 -0700

    Fixed structure

--- page_objects/brands/dashboard-brands-page.js
+++ page_objects/brands/dashboard-brands-page.js
@@ -2,10 +2,9 @@
 class Dashboard {
     // Reusable selector getters that will help us to avoid selector duplicates
     get $accountSettingsLnk() {return $('[routerlink="/creator/settings"]');}
-    get upgradeBtn() {return $('[alt="Upgrade"]');}
-    get pricingList() {return $('[class="row pricing-plan"]');}
-    get yearlyPlansBtn() {return $('[class="p-2 text-center"]');}
-    get premiumPlanTxt() {return $('[class="card-header pricing-prem-plan d-flex flex-row justify-content-between align-center"]');}
-    
+    get $upgradeBtn() {return $('[alt="Upgrade"]');}
+    get $pricingList() {return $('[class="row pricing-plan"]');}
+    get $yearlyPlansBtn() {return $('[class="p-2 text-center"]');}
+    get $premiumPlanTxt() {return $('[class="card-header pricing-prem-plan d-flex flex-row justify-content-between align-center"]');} 
 }
 module.exports = new Dashboard();
--- specs/brands/dashboard-brand-spec.js
+++ specs/brands/dashboard-brand-spec.js
@@ -6,29 +6,26 @@ const expect = require('chai').expect;
 describe('Dashboard - Brands', () => {
     it('User should be able to see 4 monthly plans on "Plans" page', () => {
         LoginPage.login({ email: 'qalesson@gmail.com', password: 'qalesson@gmail.com', portal: 'brands' });
-        // DashboardPageBrands.$accountSettingsLnk.waitForDisplayed({timeoutMsg: 'User was not able to login'});
         
-        DashboardPageBrands.upgradeBtn.waitForDisplayed();
-        DashboardPageBrands.upgradeBtn.click();
-        let plan = DashboardPageBrands.pricingList.getText();
+        DashboardPageBrands.$upgradeBtn.waitForDisplayed();
+        DashboardPageBrands.$upgradeBtn.click();
+        let plan = DashboardPageBrands.$pricingList.getText();
         expect(plan).to.contain('BASIC');
         expect(plan).to.contain('PRO');
         expect(plan).to.contain('PREMIUM');
         expect(plan).to.contain('ENTERPRISE');
-
     }),
+
     it('User should be able to see 4 yearly plans on "Plans" page', () => {
         LoginPage.login({ email: 'qalesson@gmail.com', password: 'qalesson@gmail.com', portal: 'brands' });
-        // DashboardPageBrands.$accountSettingsLnk.waitForDisplayed({timeoutMsg: 'User was not able to login'});
         
-        DashboardPageBrands.upgradeBtn.waitForDisplayed();
-        DashboardPageBrands.upgradeBtn.click();
-        DashboardPageBrands.yearlyPlansBtn.waitForDisplayed();
-        DashboardPageBrands.yearlyPlansBtn.click();
+        DashboardPageBrands.$upgradeBtn.waitForDisplayed();
+        DashboardPageBrands.$upgradeBtn.click();
+        DashboardPageBrands.$yearlyPlansBtn.waitForDisplayed();
+        DashboardPageBrands.$yearlyPlansBtn.click();
 
-        let yearlyPlan = DashboardPageBrands.pricingList.getText();
+        let yearlyPlan = DashboardPageBrands.$pricingList.getText();
         expect(yearlyPlan).to.contain('$1,000');
         expect(yearlyPlan).to.contain('$3,300');
-
     })
 })
