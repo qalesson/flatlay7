3a62c7a9a09c11537450f7eb236073ae4378a37a
Сергей Сандул <sergeysandul@MBP-Sergej.attlocal.net>
2021-09-10 10:42:23 -0700

    modified page object

--- page_objects/brands/dashboard/dashboard-brands-page.js
+++ page_objects/brands/dashboard/dashboard-brands-page.js
@@ -8,5 +8,10 @@ class DashboardBrand {
     get $discoverCreatorsByLink () {return $('span=Discover creators by');}
     get $campaignsNavigationBar () {return $('[class="d-flex flex-row justify-content-around align-items-center m-3"]');}
     get $createNewListLink () {return $('[class="create-saved-list-button link-item"]');}
+    get $accountSettingsLnk() {return $('[routerlink="/creator/settings"]');}
+    get $upgradeBtn() {return $('[alt="Upgrade"]');}
+    get $pricingList() {return $('[class="row pricing-plan"]');}
+    get $yearlyPlansBtn() {return $('[class="p-2 text-center"]');}
+    get $premiumPlanTxt() {return $('[class="card-header pricing-prem-plan d-flex flex-row justify-content-between align-center"]');} 
 }
 module.exports = new DashboardBrand();
