319329c0e531f19aa57a8268c4595a8b0cc93e94
Andrew <papapin777@gmail.com>
2021-09-11 16:26:28 -0700

    FL-32: Step 3 - Fillin  “Campaign Name” field in Create Campaign form

---
+++ page_objects/brands/dashboard/campaign/create-campaign-page.js
@@ -0,0 +1,5 @@
+"use strict";
+class CreateCampaingBrand {
+    get $inputForCampaignName () {return $('[placeholder="0/75"]');}
+}
+module.exports = new CreateCampaingBrand();
--- specs/brands/dashboard/campaign/campaign-brand-spec.js
+++ specs/brands/dashboard/campaign/campaign-brand-spec.js
@@ -1,9 +1,12 @@
 const LoginPage = require("../../../../page_objects/login-page");
 const dashboardBrandsPage = require("../../../../page_objects/brands/dashboard/dashboard-brands-page");
 const Credentials = require("../../../../data/credentials.json");
+const createCampaignPage = require("../../../../page_objects/brands/dashboard/campaign/create-campaign-page")
 
 const email = Credentials.brands.login.email;
 const password = Credentials.brands.login.password;
+
+const campaignName = "Test Campaign Name for Test Case FL-32"
 
 describe("Dashboard - Brand", () => {
   beforeEach(function () {
@@ -13,6 +16,10 @@ describe("Dashboard - Brand", () => {
   it.only("Should be able to create campaign FL-32", () => {
     dashboardBrandsPage.$createCampaignButton.waitForClickable();
     dashboardBrandsPage.$createCampaignButton.click()
+    createCampaignPage.$inputForCampaignName.waitForClickable()
+    createCampaignPage.$inputForCampaignName.setValue(campaignName);
+
+    browser.debug()
   });
 
   it("Should not be able to create campaign with past date FL-33", () => {
