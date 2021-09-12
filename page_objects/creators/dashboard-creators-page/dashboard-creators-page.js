7f83a3fe695a103cb96bab92f56e7c49844755f9
DenAU79 <iamqaman@gmail.com>
2021-09-12 16:23:35 +1000

    FL-11 new

--- page_objects/creators/dashboard-creators-page/dashboard-creators-page.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-class Dashboard {
-    // Reusable selector getters that will help us to avoid selector duplicates
-    get $accountSettingsLnk() {return $('[routerlink="/creator/settings"]');};
-    get $searchBtn() {return $('span=Search');};
-}
-module.exports = new Dashboard();
--- specs/creators/search/search-creators-spec.js
+++ specs/creators/search/search-creators-spec.js
@@ -1,8 +1,11 @@
 const LoginPage = require("../../../page_objects/login-page");
 const Credentials = require("../../../data/credentials.json");
 const { expect, assert } = require("chai");
-const SearchPage = require("../../../page_objects/creators/search-creators-page/search-page");
-const DashboardPage = require("../../../page_objects/creators/dashboard-creators-page/dashboard-creators-page");
+const SearchPage = require("../../../page_objects/creators/search/search-page");
+const DashboardPage = require("../../../page_objects/creators/dashboard/dashboard-creators-page");
+
+const email = Credentials.creators.login.email;
+const password = Credentials.creators.login.password;
 
 describe("Search", () => {
   // This hook runs befor tests
@@ -12,8 +15,11 @@ describe("Search", () => {
   });
   beforeEach(() => {
     // Go to login page and login as a Creator
-    LoginPage.login({email: Credentials.loginSpec.username, password: Credentials.loginSpec.password});
-        DashboardPage.$accountSettingsLnk.waitForDisplayed({timeoutMsg: 'User was not able to login'});    
+    LoginPage.login({email: email, password: password});
+        DashboardPage.$accountSettingsLnk.waitForDisplayed({timeoutMsg: 'User was not able to login'});
+    // Press search menu button
+    DashboardPage.$searchBtn.waitForDisplayed();
+    DashboardPage.$searchBtn.click();    
   });
   // This hook runs after each test
   afterEach(() => {
@@ -25,63 +31,10 @@ describe("Search", () => {
     browser.deleteCookies();
   });
 
-  it("FL-9 Should be able to search in ALL sections", () => {
-    // Press search menu button
-    DashboardPage.searchBtn.waitForDisplayed();
-    DashboardPage.searchBtn.click();
-    
-    // Type "ball" and press enter
-    SearchPage.search("ball");
-    browser.keys("Enter");
-
-    // Wait for search results to display
-    browser.waitUntil(() => {
-      return (SearchPage.searchResultsCreators.map((elem) => elem.isDisplayed()).length > 3);
-    },
-    { timeout: 10000, timeoutMsg: "Creators results were not visible" }
-    );
-    
-    // Verify that user can see relevant search results in Creators section
-    const searchCreators = [];
-    SearchPage.searchResultsCreators.forEach((element) => {
-      if(element.getText().length>0) searchCreators.push(element.getText().toLowerCase());
-    });        
-    expect(searchCreators.length).to.equal(4);      
-    searchCreators.every((i) => expect(i).to.contain("ball"));
-    
-    // Verify that user can see relevant search results in Feeds section  
-    const searchFeeds = [];
-    SearchPage.searchResultsFeeds.forEach((element) => {
-      if(element.length>0) searchFeeds.push(element.getText().toLowerCase());
-    });
-    expect(searchCreators.length).to.equal(4);       
-    searchFeeds.every((i) => expect(i).to.contain("ball"));
-
-    SearchPage.moveToProducts.moveTo();
-
-    // Verify that user can see relevant search results in Products section
-    browser.waitUntil(() => {
-      return (SearchPage.searchResultsProducts.map((elem) => elem.isDisplayed()).length > 3);
-    }, { timeout: 10000, timeoutMsg:'No results in Products section!'});
-    
-    // Verify that user can see relevant search results in Products section
-    const searchProducts = [];
-    SearchPage.searchResultsProducts.forEach((element) => {
-      if(element.getText().length>0) searchProducts.push(element.getText().toLowerCase());
-    });
-    console.log(searchProducts);    
-    expect(searchProducts.length).to.equal(3);      
-    searchProducts.every((i) => expect(i).to.contain("ball"));
-  });
-
   it("FL-11 Search Creators only", () => {
-    // Press search menu button
-    DashboardPage.searchBtn.waitForDisplayed();
-    DashboardPage.searchBtn.click();
-
     // Press Creators section button
-    SearchPage.searchCreatorsOnly.waitForDisplayed();
-    SearchPage.searchCreatorsOnly.click();
+    SearchPage.$searchCreatorsOnly.waitForDisplayed();
+    SearchPage.$searchCreatorsOnly.click();
 
     // Type "ball" and press enter
     SearchPage.search("ball");
@@ -89,14 +42,14 @@ describe("Search", () => {
 
     // Wait for search results to display
     browser.waitUntil(() => {
-      return (SearchPage.searchResultsCreatorsOnly.map((elem) => elem.isDisplayed()).length > 3);
+      return (SearchPage.$$searchResultsCreatorsOnly.map((elem) => elem.isDisplayed()).length > 3);
     },
     { timeout: 10000, timeoutMsg: "Creators results were not visible" }
     );
 
     // Verify that user can see relevant search results in Creators section
     const searchCreatorsOnly = [];
-    SearchPage.searchResultsCreatorsOnly.forEach((element) => {
+    SearchPage.$$searchResultsCreatorsOnly.forEach((element) => {
       if(element.getText().length>0) searchCreatorsOnly.push(element.getText().toLowerCase());
     });    
     expect(searchCreatorsOnly.length).to.equal(10);      
