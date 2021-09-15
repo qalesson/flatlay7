547f7b610c46ccf448f4b4d7215bedb882935409
Eleonora Arutyunova <ella.a@Eleonoras-Air.attlocal.net>
2021-09-15 12:16:47 -0700

    Combined FL 28, FL 29, FL 30

--- page_objects/brands/dashboard/dashboard-brands-page.js
+++ page_objects/brands/dashboard/dashboard-brands-page.js
@@ -8,5 +8,8 @@ class DashboardBrand {
     get $discoverCreatorsByLink () {return $('span=Discover creators by');}
     get $campaignsNavigationBar () {return $('[class="d-flex flex-row justify-content-around align-items-center m-3"]');}
     get $createNewListLink () {return $('[class="create-saved-list-button link-item"]');}
+    get $profileImage () {return $('[class="profile-photo"]');}
+    get $incorrectUsernameError () {return $('[role="alertdialog"]');}
+    get $signOutButton () {return $('button=Sign Out');}
 }
 module.exports = new DashboardBrand();
--- specs/brands/login/login-brands-spec.js
+++ specs/brands/login/login-brands-spec.js
@@ -5,6 +5,9 @@ const Credentials = require("../../../data/credentials.json")
 const email = Credentials.brands.login.email;
 const password = Credentials.brands.login.password;
 
+const emailTest = 'test@gmail.com';
+const passwordTest = 'password';
+
 describe('Login - Brand', () => {
     it('should be able to login with matching credentials FL-28', () => {
         LoginPage.login({ email: email, password: password, portal: 'brands' });
@@ -12,5 +15,19 @@ describe('Login - Brand', () => {
 
     });
 
+    it('should be able to sign out FL-29', () => {
+        LoginPage.login({ email: email, password: password, portal: 'brands' });
+        DashboardBrandsPage.$campaignsButton.waitForDisplayed();
+        DashboardBrandsPage.$profileImage.click();
+        DashboardBrandsPage.$signOutButton.waitForDisplayed ();
+        DashboardBrandsPage.$signOutButton.click();
+
+    });
+
+    it('should not be able to login with incorrect credentials FL-30', () => {
+        LoginPage.login({ email: emailTest, password: passwordTest, portal: 'brands' });
+        DashboardBrandsPage.$incorrectUsernameError.waitForDisplayed();
+    
+        });
 
 })
