facca71bebb0f7aea6c0d08d21cbf72cdbd20a72
Eleonora Arutyunova <ella.a@Eleonoras-Air.attlocal.net>
2021-09-19 00:12:33 -0700

    Submit for a review FL-73

---
+++ specs/creators/login/registration-creators-spec.js
@@ -0,0 +1,13 @@
+const RegistrationPage = require('../../../page_objects/registration-page');
+const Credentials = require('../../../data/credentials.json');
+
+const email = Credentials.creatorRegistrationTest.login.email;
+const password = Credentials.creatorRegistrationTest.login.password;
+
+describe('Registration - Creators', () => {
+    it('should not be able to register with no username FL-73', () => {
+        browser.url('https://new.flatlay.io/register');
+        RegistrationPage.login({ email: email, password: password });
+        RegistrationPage.$UsernameRequiredError.waitForDisplayed ();
+    })
+})
