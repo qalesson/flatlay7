84776d83d39e71ee5d46c9c59089e2ac33c27e33
Sergii Khromchenko <khromchenko13@gmail.com>
2021-09-27 13:31:16 -0700

    cleanup

--- specs/creators/login/login-creators-spec.js
+++ specs/creators/login/login-creators-spec.js
@@ -1,6 +1,6 @@
 const LoginPage = require('../../../page_objects/login-page');
 const DashboardPage = require('../../../page_objects/creators/dashboard/dashboard-creators-page');
-const Credentials = require('../../../data/credentials.json');
+const Credentials = require('../../../data/Credentials.json');
 
 const email = Credentials.creators.login.email;
 const password = Credentials.creators.login.password;
