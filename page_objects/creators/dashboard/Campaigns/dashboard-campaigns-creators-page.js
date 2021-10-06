94b57f1b901d3eee6a899aee80de49998e5167a9
kosarsute <kosarsute@gmail.com>
2021-10-06 16:27:47 -0700

    hmm wtheck

--- page_objects/creators/dashboard/Campaigns/dashboard-campaigns-creators-page.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-class DashboardCreatorsCampaigns {
-    // Reusable selector getters that will help us to avoid selector duplicates
-    get $campaignsNavBotton() {return $('[routerlink="campaigns"]');};
-    get $campaignsSearchBox() {return $('[placeholder="Search for campaigns"]');};
-    get $campaignsSearchResult() {return $('span=You are the best');}
-    get $campaignsSearchResultEmpty() {return $('p=No active campaigns');}
-}
-module.exports = new DashboardCreatorsCampaigns();
