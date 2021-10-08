feae8418682cee21171964a67d479870b19d8ae8
DenAU79 <iamqaman@gmail.com>
2021-10-08 15:02:29 +1100

    FL-61 updated

--- page_objects/brands/campaign/campaigns-page.js
+++ page_objects/brands/campaign/campaigns-page.js
@@ -3,7 +3,36 @@ class CampaignsBrand {
   get $$campaignStatusLbl() {return $$('.campaign-status');}
   get $activeCampaignBtn () {return $('.status-icon-active');}
   get $editCampaignDateBtn () {return $('[class="row w-100 p-2"] div:nth-child(1) .text-dark-green');}
-  get $publishedCampaignStartCalendar () {return $('[disabled="true"]');}
+  get $draftCampaignStartCalendar () {return $('//*[@formcontrolname="startdate"]/../following-sibling::div');}
+  get $draftCampaignEndCalendar () {return $('//*[@formcontrolname="enddate"]/../following-sibling::div');}
+  get $chooseDateBtn () {return $('[aria-label="Choose month and year"]');}
+  get $currentDateBtn () {return $('.mat-calendar-body-today');}
+  get $tomorrowDateBtn () {return $('//*[@class="mat-calendar-body-cell-content mat-calendar-body-today"]/../following-sibling::td');}
+  get $saveDateBtn () {return $('.btn-save');}
+  get $saveDateMsg () {return $('[role="alertdialog"]');}
+
+  selectCurrentDate() {
+    this.$chooseDateBtn.waitForDisplayed();
+    this.$chooseDateBtn.click();
+    this.$currentDateBtn.waitForDisplayed();
+    this.$currentDateBtn.click();
+    this.$currentDateBtn.waitForDisplayed();
+    this.$currentDateBtn.click();
+    this.$currentDateBtn.waitForDisplayed();
+    this.$currentDateBtn.click();
+  };
+
+  selectTomorrowDate() {
+    this.$chooseDateBtn.waitForDisplayed();
+    this.$chooseDateBtn.click();
+    this.$currentDateBtn.waitForDisplayed();
+    this.$currentDateBtn.click();
+    this.$currentDateBtn.waitForDisplayed();
+    this.$currentDateBtn.click();
+    this.$tomorrowDateBtn.waitForDisplayed();
+    this.$tomorrowDateBtn.click();
+  };
+
 }
 
 module.exports = new CampaignsBrand(); 
--- specs/brands/campaign/campaign-brand-spec.js
+++ specs/brands/campaign/campaign-brand-spec.js
@@ -1,32 +1,37 @@
 const LoginPage = require("../../../page_objects/login-page");
-const DashboardBrandsPage = require("../../../page_objects/brands/dashboard/dashboard-brands-page");
 const Credentials = require("../../../data/Credentials.json");
 const CampaignsPage = require("../../../page_objects/brands/campaign/campaigns-page");
 const { expect } = require("chai");
 
-    const email = Credentials.brands.login.email;
-    const password = Credentials.brands.login.password;
+const email = Credentials.brands.login.email;
+const password = Credentials.brands.login.password;
 
-describe("Dashboard - Brand", () => {  
-    beforeEach(function () {
-      LoginPage.login({ email: email, password: password, portal: "brands" });
-    });
+describe("Dashboard - Brand", () => {
+  beforeEach(function () {
+    LoginPage.login({ email: email, password: password, portal: "brands" });
+  });
 
-    it("FL-61 Should not be able to change start day of the published campaign", () => {
-        // Click Campaigns menu btn
-       DashboardBrandsPage.$campaignsButton.waitForDisplayed();
-       DashboardBrandsPage.$campaignsButton.click();
+  it("FL-61 Should be able to change start and end day of the draft campaign", () => {
+    // Click on Edit campaign under the "Days left" section
+    CampaignsPage.$editCampaignDateBtn.waitForDisplayed();
+    CampaignsPage.$editCampaignDateBtn.click();
 
-       // Click on Active campaign
-       CampaignsPage.$activeCampaignBtn.waitForDisplayed();
-       CampaignsPage.$activeCampaignBtn.click();
-       
-       // Click on Edit campaign under the "Days left" section
-       CampaignsPage.$editCampaignDateBtn.waitForDisplayed();
-       CampaignsPage.$editCampaignDateBtn.click();       
+    // Choose campaign start day in the calendar
+    CampaignsPage.$draftCampaignStartCalendar.waitForDisplayed();
+    CampaignsPage.$draftCampaignStartCalendar.click();
+    CampaignsPage.selectCurrentDate();
 
-       // Verify that user is not able to change a start day
-       CampaignsPage.$publishedCampaignStartCalendar.waitForDisplayed();
-       expect(CampaignsPage.$publishedCampaignStartCalendar.isClickable()).to.be.false;
-      });
-    });
+    // Choose campaign end day in the calendar
+    CampaignsPage.$draftCampaignEndCalendar.click();
+    CampaignsPage.selectTomorrowDate();
+
+    // Save campaign dates
+    CampaignsPage.$saveDateBtn.click();
+
+    //Verify that dates have been changed
+    CampaignsPage.$saveDateMsg.waitForDisplayed();
+    expect(CampaignsPage.$saveDateMsg.getText()).to.equal(
+      "Your campaign information has been successfully changed"
+    );
+  });
+});
