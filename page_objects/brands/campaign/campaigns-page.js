8ebda0ad62df946be03b5ede67fcfdbf5ebec497
kosarsute <kosarsute@gmail.com>
2021-10-11 18:36:52 -0700

    helper

---
+++ page_objects/brands/campaign/campaigns-page.js
@@ -0,0 +1,9 @@
+"use strict";
+class CampaignsBrand {  
+  get $$campaignStatusLbl() {return $$('.campaign-status');}
+  get $activeCampaignBtn () {return $('.status-icon-active');}
+  get $editCampaignDateBtn () {return $('[class="row w-100 p-2"] div:nth-child(1) .text-dark-green');}
+  get $publishedCampaignStartCalendar () {return $('[disabled="true"]');}
+}
+
+module.exports = new CampaignsBrand(); 
--- page_objects/brands/dashboard/dashboard-brands-page.js
+++ page_objects/brands/dashboard/dashboard-brands-page.js
@@ -1,21 +1,46 @@
 "use strict";
 class DashboardBrand {
-    get $homeButton () {return $('[routerlink="/brand/home"]');}
-    get $discoverButton () {return $('[routerlink="/brand/discover"]');}
-    get $campaignsButton () {return $('[routerlink="/brand/campaign"]');}
-    get $savedButton () {return $('[routerlink="/brand/saved"]');}
-    get $currentUserName () {return $('[class="current-user-name"]');}
-    get $discoverCreatorsByLink () {return $('span=Discover creators by');}
-    get $campaignsNavigationBar () {return $('[class="d-flex flex-row justify-content-around align-items-center m-3"]');}
-    get $createNewListLink () {return $('[class="create-saved-list-button link-item"]');}
-    get $profileImage () {return $('[class="profile-photo"]');}
-    get $incorrectUsernameError () {return $('[role="alertdialog"]');}
-    get $signOutButton () {return $('button=Sign Out');}
-    get $accountSettingsLnk() {return $('[routerlink="/creator/settings"]');}
-    get $upgradeBtn() {return $('[alt="Upgrade"]');}
-    get $pricingListLbl() {return $('[class="row pricing-plan"]');}
-    get $$basicEnterprisePlanLbl() {return $$('[class="col-sm-6 col-xs-12 p-2 col-md-3 ng-star-inserted"]');}
-    get $$proPremiumPlanLbl() {return $$('[class="col-sm-6 col-xs-12 p-2 col-md-3"]');}
-    get $$plansLbl() {return $$('.col-sm-6.col-xs-12');}
+    get $homeButton() { return $('[routerlink="/brand/home"]'); }
+    get $discoverButton() { return $('[routerlink="/brand/discover"]'); }
+    get $campaignsButton() { return $('a=Campaigns'); }
+    get $listButton() { return $('[routerlink="/brand/list"]'); }
+    get $currentUserName() { return $('[class="current-user-name"]'); }
+    get $discoverCreatorsByLink() { return $('span=Discover creators by'); }
+    get $campaignsNavigationBar() { return $('[class="d-flex flex-row justify-content-around align-items-center m-3"]'); }
+    get $createNewListLink() { return $('[class="create-saved-list-button link-item"]'); }
+    get $profileImage() { return $('[class="profile-photo"]'); }
+    get $settignsFromImageBtn() { return $('button=Settings'); }
+    get $changePlanBtn() { return $('[class="link-items link-item"]'); }
+    get $incorrectUsernameError() { return $('[role="alertdialog"]'); }
+    get $signOutButton() { return $('button=Sign Out'); }
+    get $accountSettingsLnk() { return $('[routerlink="/creator/settings"]'); }
+    get $upgradeBtn() { return $('[alt="Upgrade"]'); }
+    get $yearlyPlansBtn() { return $('[class="p-2 text-center"]'); }
+    get $$basicEnterprisePlanLbl() { return $$('[class="col-sm-6 col-xs-12 p-2 col-md-3 ng-star-inserted"]'); }
+    get $$proPremiumPlanLbl() { return $$('[class="col-sm-6 col-xs-12 p-2 col-md-3"]'); }
+    get $$plansLbl() { return $$('.col-sm-6.col-xs-12'); }
+    get socialAccountsList() { return $$('.social-item'); }
+
+    changePlanPage() {
+        // Click on Profile image
+        this.$profileImage.waitForClickable();
+        this.$profileImage.click();
+        // Click on Settings button
+        this.$settignsFromImageBtn.waitForDisplayed();
+        this.$settignsFromImageBtn.click();
+        // Click on Change Plan button
+        this.$changePlanBtn.waitForDisplayed();
+        this.$changePlanBtn.click();
+    }
+    connectSocialAccountByText(socialAccountText) {
+        const socialAccountItems = {};
+        browser.waitUntil(() => {
+            return this.socialAccountsList.map((elem) => elem.isDisplayed()).length > 6;
+        }, { timeout: 10000, timeoutMsg: 'Social were not visible' });
+        this.socialAccountsList.forEach(element => {
+            socialAccountItems[element.$('.section-title').getText().toLowerCase()] = element.$('.ml-auto');
+        });
+        socialAccountItems[socialAccountText].click();
+    }
 }
 module.exports = new DashboardBrand();
