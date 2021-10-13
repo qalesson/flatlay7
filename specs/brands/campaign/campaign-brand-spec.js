41abc676a4d0eca9e863d2fb06df4cb21dc5c19a
Сергей Сандул <sergeysandul@MBP-Sergej.attlocal.net>
2021-10-13 12:11:40 -0700

    resolved conflicts2

--- specs/brands/campaign/campaign-brand-spec.js
+++ specs/brands/campaign/campaign-brand-spec.js
@@ -4,64 +4,23 @@ const CampaignsPage = require("../../../page_objects/brands/campaign/campaigns-p
 const SettingsPage = require("../../../page_objects/brands/settings/settings-page")
 const { expect } = require("chai");
 
-<<<<<<< HEAD
-const email = Credentials.brands.login.email;
-const password = Credentials.brands.login.password;
-=======
 const email = Credentials.brands.campaigns.email;
 const password = Credentials.brands.campaigns.password;
->>>>>>> 1dcc6927cd639a836c5e1cfd1676ef8a43bbe85c
 
 describe("Dashboard - Brand", () => {
   beforeEach(function () {
     LoginPage.login({ email: email, password: password, portal: "brands" });
   });
 
-<<<<<<< HEAD
-  it("FL-61 Should not be able to change start day of the published campaign", () => {
-    // Click Campaigns menu btn
-    DashboardBrandsPage.$campaignsButton.waitForDisplayed();
-    DashboardBrandsPage.$campaignsButton.click();
-
-    // Click on Active campaign
-    CampaignsPage.$activeCampaignBtn.waitForDisplayed();
-    CampaignsPage.$activeCampaignBtn.click();
-=======
   it("FL-61 Should be able to change start and end day of the draft campaign", () => {    
     // Choose the draft campaign
     CampaignsPage.$draftCampaignBtn.waitForDisplayed();
     CampaignsPage.$draftCampaignBtn.click();
->>>>>>> 1dcc6927cd639a836c5e1cfd1676ef8a43bbe85c
 
     // Click on Edit campaign under the "Days left" section
     CampaignsPage.$editCampaignDateBtn.waitForDisplayed();
     CampaignsPage.$editCampaignDateBtn.click();
 
-<<<<<<< HEAD
-    // Verify that user is not able to change a start  day
-    CampaignsPage.$publishedCampaignStartCalendar.waitForDisplayed();
-    expect(CampaignsPage.$publishedCampaignStartCalendar.isClickable()).to.be.false;
-  });
-
-  it("FL-61 Should not be able to change start day of the published campaign", () => {
-    // Click Campaigns menu btn
-    DashboardBrandsPage.$campaignsButton.waitForDisplayed();
-    DashboardBrandsPage.$campaignsButton.click();
-
-    // Click on Active campaign
-    CampaignsPage.$activeCampaignBtn.waitForDisplayed();
-    CampaignsPage.$activeCampaignBtn.click();
-
-    // Click on Edit campaign under the "Days left" section
-    CampaignsPage.$editCampaignDateBtn.waitForDisplayed();
-    CampaignsPage.$editCampaignDateBtn.click();
-
-    // Verify that user is not able to change a start  day
-    CampaignsPage.$publishedCampaignStartCalendar.waitForDisplayed();
-    expect(CampaignsPage.$publishedCampaignStartCalendar.isClickable()).to.be.false;
-  });
-});
-=======
     // Choose campaign start day in the calendar
     CampaignsPage.$draftCampaignStartCalendar.waitForDisplayed();
     CampaignsPage.$draftCampaignStartCalendar.click();
@@ -81,4 +40,3 @@ describe("Dashboard - Brand", () => {
     );
   });
 });
->>>>>>> 1dcc6927cd639a836c5e1cfd1676ef8a43bbe85c
