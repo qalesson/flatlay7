5e6d4c4b89aac887bd54e4f3415440f11088c3b1
kosarsute <kosarsute@gmail.com>
2021-10-12 23:37:17 -0700

    homework

--- page_objects/brands/discover/dashboard-brand-discover.js
+++ page_objects/brands/discover/dashboard-brand-discover.js
@@ -1,7 +1,27 @@
 "use strict";
 class DashboardBrandDiscover {
-    get $sliderMax () { return $('[class="ngx-slider-span ngx-slider-pointer ngx-slider-pointer-max"]'); }
-    get $applyFiltersBtn () { return $('[class="btn btn-block btn-apply-filters mx-2"]'); }
-    get $$creatorsData () { return $$('[class="header-title"]'); }
+    get $sliderMax() { return $('[class="ngx-slider-span ngx-slider-pointer ngx-slider-pointer-max"]'); }
+    get $applyFiltersBtn() { return $('[class="btn btn-block btn-apply-filters mx-2"]'); }
+    get $$creatorsData() { return $$('[class="header-title"]'); }
+    get $$socialAccountsSidebar() { return $$('[class="col-sm-4 d-flex flex-row justify-content-between align-center"] [class="content-title"]'); }
+    get $$socialAccountsSidebar1 () { return $$('[class="col-sm-4 d-flex flex-row justify-content-between align-center"] [class="header-title"]'); }
+    get $$topMenu () { return $$('[class="col-sm-4 d-flex flex-row justify-content-between align-center"] [class="content-title"]');}
+    get $$followersCount () { return $$('[class="col-sm-4 d-flex flex-row justify-content-between align-center"] [class="header-title"]');}
+
+    verifyNameUsername(menuItemText) {
+        const datas = {};
+        browser.waitUntil(() => {
+          return this.$$topMenu.map((elem) => elem.isDisplayed()).length > 2;
+      }, { timeout: 10000, timeoutMsg:'Ten elements were not visible'});
+        this.$$topMenu.forEach(element => {
+           datas[element.getText()] = element;
+           
+        });
+        datas[menuItemText].isDisplayed();
+        if ( datas[menuItemText] === 'Engagement' )
+        {
+            expect(datas[menuItemText].length).to.be.above(3)
+        } 
+    }
 }
 module.exports = new DashboardBrandDiscover();
--- specs/brands/discover/discover-brand-spec.js
+++ specs/brands/discover/discover-brand-spec.js
@@ -4,25 +4,27 @@ const Credentials = require("../../../data/Credentials.json");
 const DashboardBrandDiscover = require('../../../page_objects/brands/discover/dashboard-brand-discover');
 
 const { expect } = require('chai');
+const dashboardBrandDiscover = require('../../../page_objects/brands/discover/dashboard-brand-discover');
 
 const email = Credentials.brands.login.email;
 const password = Credentials.brands.login.password;
 
-describe.skip('Dashboard - Brand', () => {
+
+describe('Dashboard - Brand', () => {
     beforeEach(function () {
         LoginPage.login({ email: email, password: password, portal: 'brands' });
     });
 
-    it('Brand can filter creators by followers count FL-70', () => {
+    it.skip('Brand can filter creators by followers count FL-70', () => {
         DashboardBrandsPage.$discoverButton.waitForClickable();
         DashboardBrandsPage.$discoverButton.click();
         DashboardBrandsPage.$discoverCreatorsByLink.waitForDisplayed();
-        DashboardBrandDiscover.$sliderMax.waitForClickable({timeout: 30000});
+        DashboardBrandDiscover.$sliderMax.waitForClickable({ timeout: 30000 });
         DashboardBrandDiscover.$sliderMax.dragAndDrop({ x: -225, y: 0 })
         DashboardBrandDiscover.$applyFiltersBtn.click()
         browser.waitUntil(() => {
-            return  DashboardBrandDiscover.$$creatorsData.map((elem) => elem.isDisplayed()).length > 5;
-        }, { timeout: 10000, timeoutMsg:'5 users has not been displayed'});
+            return DashboardBrandDiscover.$$creatorsData.map((elem) => elem.isDisplayed()).length > 5;
+        }, { timeout: 10000, timeoutMsg: '5 users has not been displayed' });
         const creatorName = [];
         DashboardBrandDiscover.$$creatorsData.forEach(element => {
             creatorName.push(element.getText().toLowerCase());
@@ -38,4 +40,23 @@ describe.skip('Dashboard - Brand', () => {
         expect(parseInt(creatorName[33])).to.be.below(25);
         expect(parseInt(creatorName[37])).to.be.below(25);
     });
+    it('List of creators contains name/username, Followers lavel + amount, Following label + amount, Engagement label FL-81', () => {
+       $('a=Discover').click()
+        browser.pause(3000);
+        let name;
+        let username;
+        let followersCount;
+        $$('[class="row discover-campaign-main-section p-3 align-center"]').forEach((element) => {
+            // name = element.$('[class="ml-1 link-item"] [class="header-title"]').getText();
+            // someone created usernames without names so it's a bug
+            username = element.$('[class="ml-1 link-item"] [class="content-title"]').getText();
+            followersCount = element.$('[class="col-sm-4 d-flex flex-row justify-content-between align-center"] [class="header-title"]').getText();
+            expect(parseInt(followersCount)).to.be.above(1)
+            // expect(name.length).to.be.above(1);
+            expect(username.length).to.be.above(1);
+            dashboardBrandDiscover.verifyNameUsername("Following")
+            dashboardBrandDiscover.verifyNameUsername("Followers")
+            dashboardBrandDiscover.verifyNameUsername("Engagement")
+        });
+    })
 })
